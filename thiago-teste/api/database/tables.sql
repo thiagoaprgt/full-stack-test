158c2bc1581d05d55c86a8a044a311cbe948f607
Thiago <thiago_ap_@hotmail.com>
2024-11-03 16:55:54 -0300

    ajustes no arquivo sql

--- thiago-teste/api/database/tables.sql
+++ thiago-teste/api/database/tables.sql
@@ -1,9 +1,20 @@
+create table users (
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name varchar(100) not null,
+    email varchar(100) not null,
+    password varchar(100) not null
+)
+
 create table taskToDoList (
 
-    id int(100) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     title varchar(100) not null,
-    description varchar(10000) not null
+    description varchar(1000) not null,
+    user_id int not null,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 
 );
 
-INSERT INTO `taskToDoList`(`title`, `description`) VALUES ('tarefa1','descrição1') , ('tarefa2','fasfasdfas');
+INSERT INTO `users`( `name`, `email`, `password`) VALUES ('teste','teste@email','456'), ('testando','testando@email','123')
+
+INSERT INTO `taskToDoList`(`title`, `description`, `user_id`) VALUES ('tarefa1','descrição1', 1) , ('tarefa2','fasfasdfas', 1);
