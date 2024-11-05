eacb10093a4c34c2f9e5cbba0ea5303bdd1f81cf
Thiago <thiago_ap_@hotmail.com>
2024-11-05 16:38:10 -0300

    ajuste no maix.tsx

--- thiago-teste/src/main.tsx
+++ thiago-teste/src/main.tsx
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 
-import { AppLogoutRoutes } from './AppLogoutRoutes.tsx';
+import { AppLogoutRoutes } from './appLogoutRoutes.tsx';
 
 
 createRoot(document.getElementById('root')!).render(
