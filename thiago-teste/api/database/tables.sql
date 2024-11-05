44cdd78883baf5c95a6ea1250a6953fdd938b2c1
Thiago <thiago_ap_@hotmail.com>
2024-11-05 15:11:45 -0300

    Ajustes no arquivo sql

--- thiago-teste/api/database/tables.sql
+++ thiago-teste/api/database/tables.sql
@@ -16,6 +16,6 @@ create table taskToDoList (
 
 );
 
-INSERT INTO `users`( `name`, `users`, `email`, `password`) VALUES ('Thiago teste', 'teste','teste@email','456'), ('testando','testando@email','123');
+INSERT INTO `users`( `name`, `users`, `email`, `password`) VALUES ('Thiago teste', 'teste','teste@email','456'), ('Carla', 'testando','testando@email','123');
 
 INSERT INTO `taskToDoList`(`title`, `description`, `user_id`) VALUES ('tarefa1','descrição1', 1) , ('tarefa2','fasfasdfas', 1);
