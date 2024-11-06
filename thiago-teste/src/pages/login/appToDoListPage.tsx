5f92fee786552930935eb0050b5581b243203be4
Thiago <thiago_ap_@hotmail.com>
2024-11-06 19:44:25 -0300

    esboço do layout do página toDoList

--- thiago-teste/src/pages/login/appToDoListPage.tsx
+++ thiago-teste/src/pages/login/appToDoListPage.tsx
@@ -7,6 +7,32 @@ export function AppToDoListPage() {
             <div>
                 Funcionando a página App to Do List
             </div>
+
+
+            <div className={styles.toDoList}>
+
+                <div id="newTaskForm" className={styles.newTask}>
+
+
+
+                </div>
+
+                <div id="taskProgress" className={styles.taskProgress}>
+
+                    <div id="toDo" className={styles.toDo}>
+
+                    </div>
+
+                    <div id="inProgress" className={styles.inProgress}>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
         </>
     );
 }
