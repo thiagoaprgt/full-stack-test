0489be76378b16aaa35dc178f641a290c790bb9d
Thiago <thiago_ap_@hotmail.com>
2024-11-06 21:44:20 -0300

    ajustando o layout

---
+++ thiago-teste/src/pages/login/toDo/appToDoListPage.tsx
@@ -0,0 +1,96 @@
+import styles from './appToDoListPage.module.css'
+
+
+export function AppToDoListPage() {
+
+    let closetoTaskForm = async () => {
+
+         /*
+            a função do javascritp eval() valida a string como um comando javascript,
+            foi aqui pra que o react não de erro na proprieda .style
+        */
+            eval(`document.querySelectorAll('#createTaskToDoFormBackground')[0].style.visibility = "hidden"`);
+
+    }
+
+    let addNewTaskToDo = async () => {
+
+    }
+
+    return(
+        <>
+
+
+            <div id="createTaskToDoFormBackground" className={styles.createTaskToDoFormBackground}>
+
+                <div className={styles.createTaskToDoForm}>
+
+                    <div id="closetoTaskForm" className={styles.closetoTaskForm} >
+                        <span onClick={closetoTaskForm}>Close</span>
+                    </div>
+
+                    <div className={styles.inputsCreateTaskToDoForm}>
+                        
+                        <div className={styles.formInputs}>
+                            <div>Título da nova tarefa:</div>
+                            <input id="taskTitle" type="text" />
+                            
+                        </div>
+
+                        <div className={styles.formInputs}>
+                            <div>Descrição da nova tarefa:</div>
+                            <input id="taskDescription"  type="password" />
+                        </div>
+
+                        <div onClick={addNewTaskToDo} id="addNewTaskToDo" className={styles.addNewTaskToDoButton}>
+
+                            <div >Adicionar Nova Tarefa</div>
+                            
+                        </div> 
+
+                    </div>
+
+                </div>                
+
+            </div>
+            
+
+
+            <div className={styles.toDoList}>
+
+                <div id="newTaskForm" className={styles.newTask}>
+
+                    Clique na coluna to Do para criar uma nova task
+
+                </div>
+
+                <div id="taskProgress" className={styles.taskProgress}>
+
+                    <div id="toDo" className={styles.toDo}>
+
+                        <p>
+                            <span>To Do</span>
+                            
+                        </p>
+
+                        
+
+                    </div>
+
+                    <div id="inProgress" className={styles.inProgress}>
+
+                        <p>
+                            In Progress
+                        </p>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+        </>
+    );
+}
