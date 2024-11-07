321c8fefde8f5bd7a28dc4edc2fa5102fdf21201
Thiago <thiago_ap_@hotmail.com>
2024-11-06 23:43:23 -0300

    ajuste no arquivo sql

--- thiago-teste/api/database/tables.sql
+++ thiago-teste/api/database/tables.sql
@@ -1,9 +1,14 @@
 create table users (
     id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name varchar(100) not null,
-    user varchar(100) not null,
+    user varchar(100) not null unique,
     email varchar(100) not null unique,
     password varchar(100) not null
+);
+
+create table taskProgress(
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name varchar(100) not null
 );
 
 create table taskToDoList (
@@ -11,11 +16,16 @@ create table taskToDoList (
     id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     title varchar(100) not null,
     description varchar(1000) not null,
+    taskProgress_id int not null,
+    FOREIGN KEY (taskProgress_id) REFERENCES taskProgress(id),
+
     user_id int not null,
     FOREIGN KEY (user_id) REFERENCES users(id)
 
 );
 
-INSERT INTO `users`( `name`, `user`, `email`, `password`) VALUES ('Thiago teste', 'teste','teste@email','456'), ('Carla', 'testando','testando@email','123');
+INSERT INTO `users`( `name`, `user`, `email`, `password`) VALUES ('Thiago teste', 'teste', 'teste@email','456'), ('Carla', 'testando', 'testando@email','123');
 
-INSERT INTO `taskToDoList`(`title`, `description`, `user_id`) VALUES ('tarefa1','descrição1', 1) , ('tarefa2','fasfasdfas', 1);
+INSERT INTO `taskProgress` (`name`) VALUES ('toDo'), ('inProgres');
+
+INSERT INTO `taskToDoList`(`title`, `description`, `user_id`, `taskProgress_id`) VALUES ('tarefa1','descrição1', 1, 1) , ('tarefa2','fasfasdfas', 1, 2);
