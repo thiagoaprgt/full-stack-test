3cf4f4f78a01bb5fc7e0127b5ba561781f5fb21a
Thiago <thiago_ap_@hotmail.com>
2024-11-10 19:11:35 -0300

    arquivo de configuração de email

---
+++ thiago-teste/api/emailConfig.js
@@ -0,0 +1,8 @@
+email = {
+    driver: 'smtp',
+    emailAddress: 'suporte.thiago.ap.profissional@gmail.com',
+    smtpServer: 'smtp.gmail.com',
+    passwordSmptServer: 'coloque a senha que foi gerada no servidor smtp',
+    port: 587,
+    encription: 'TLS'
+}
