174320cf8c3cebc51ccafd62a2f55c9e7d808eb1
Thiago <thiago_ap_@hotmail.com>
2024-11-10 20:42:04 -0300

    deletando o arquivo configuração do email

--- thiago-teste/api/emailConfig.js
+++
@@ -1,8 +0,0 @@
-email = {
-    driver: 'smtp',
-    emailAddress: 'suporte.thiago.ap.profissional@gmail.com',
-    smtpServer: 'smtp.gmail.com',
-    passwordSmptServer: 'coloque a senha servidor smtp',
-    port: 587,
-    encription: 'TLS'
-}
